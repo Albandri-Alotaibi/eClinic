439752d35f6b1ef6d45b0abfdfae5fbe3b9a7b6e
DeemAt10 <d-10-16@hotmail.com>
2022-11-26 19:08:47 +0300

    Update deem.dart

--- eclinic_application/lib/deem.dart
+++ eclinic_application/lib/deem.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-class deem extends StatelessWidget{
+class deem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('add hours'),
+      ),
+    );
   }
-
 }
