2b9d6a020ef73d8ac468070835e0710d2fbc53a1
Alhanouf12 <93281161+Alhanouf12@users.noreply.github.com>
2022-12-05 23:57:43 +0300

    Update facultysignup.dart

--- eclinic_application/lib/facultysignup.dart
+++ eclinic_application/lib/facultysignup.dart
@@ -187,9 +187,9 @@ class _facultysignupState extends State<facultysignup> {
 
   checkidspecialty(List<String?> specialityoption) async {
     speciality.length = 0;
-    print(specialityoption);
-    print(speciality.length);
-    print(speciality);
+    // print(specialityoption);
+    // print(speciality.length);
+    // print(speciality);
 
     try {
       await FirebaseFirestore.instance
