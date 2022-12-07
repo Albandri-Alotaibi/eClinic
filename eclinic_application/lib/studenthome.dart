731dabff1ce14e34f21ec5a3dc79bb24c7bf2919
DeemAt10 <d-10-16@hotmail.com>
2022-12-07 12:19:29 +0300

    student and yaml

--- eclinic_application/lib/studenthome.dart
+++ eclinic_application/lib/studenthome.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class studenthome extends StatefulWidget {
   // This class is the configuration for the state.
@@ -41,4 +47,141 @@ class _sState extends State<studenthome> {
       ],
     );
   }
+
+  //+++++++++++++++++++++++++++++++++++++++++DEEM+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  void requestPremission() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+      print("User granted premission");
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+      print("User granted provisional permission");
+    } else {
+      print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+      print("User declined or has not accepted premission");
+    }
+  }
+
+  //+++++++++++++++++++++++++++++++++++++DEEM++++++++++++++++++++++++++++++++
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  TextEditingController username = TextEditingController();
+  TextEditingController title = TextEditingController();
+  TextEditingController body = TextEditingController();
+  //+++++++++++++++++++++++++++++++++++++DEEM++++++++++++++++++++++++++++++++
+
+  void initState() {
+    super.initState();
+
+    //++++++++++++++++++++++++++DEEM++++++++++++++++++++++++++++++++
+    requestPremission();
+    getToken();
+    // initInfo();
+  }
+
+  String? mtoken = " ";
+
+  void getToken() async {
+    await FirebaseMessaging.instance.getToken().then((token) {
+      setState(() {
+        mtoken = token;
+        print("My token is $mtoken");
+      });
+      saveToken(token!);
+    });
+  }
+
+  void saveToken(String token) async {
+    await FirebaseFirestore.instance
+        .collection("student")
+        .doc("8aeGvoqjmTtCYhVcaLBm")
+        .update({'token': token});
+  }
+
+  // initInfo() {
+  //   var androidInitialize =
+  //       const AndroidInitializationSettings("@mipmap/ic_launcher");
+  //   var initializationSettings =
+  //       InitializationSettings(android: androidInitialize);
+
+  //   flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  //   FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+  //     print("...................onMessage................");
+  //     print(
+  //         "onMessage: ${message.notification?.title}/${message.notification?.body}");
+
+  //     BigTextStyleInformation bigTextStyleInformation = BigTextStyleInformation(
+  //         message.notification!.body.toString(),
+  //         htmlFormatBigText: true,
+  //         contentTitle: message.notification!.title.toString(),
+  //         htmlFormatContentTitle: true);
+
+  //     AndroidNotificationDetails androidPlatformChannelSpecifics =
+  //         AndroidNotificationDetails(
+  //       "dbfood",
+  //       "dbfood",
+  //       importance: Importance.high,
+  //       styleInformation: bigTextStyleInformation,
+  //       priority: Priority.high,
+  //       playSound: true,
+  //     );
+
+  //     NotificationDetails platformChannelSpecifics =
+  //         NotificationDetails(android: androidPlatformChannelSpecifics);
+  //     await flutterLocalNotificationsPlugin.show(0, message.notification?.title,
+  //         message.notification?.body, platformChannelSpecifics,
+  //         payload: message.data['body']);
+  //   });
+  // }
+
+  // void sendPushMessege(String token) async {
+  //   try {
+  //     await http.post(
+  //       Uri.parse('https://fcm.googleapis.com/fcm/send'),
+  //       headers: <String, String>{
+  //         'Content-Type': 'application/json',
+  //         'Authorization':
+  //             'key=AAAAIqZZfrY:APA91bGmyg-TtTdQSaXgauRdN1LmyCWHL18IWSzI5UxqHk3TYdCFegHCDysMhyEvEYFBi9o7ofyiAQE-HZOG_TGH9AnfknXWUZmZpbKvBJ0Wx4zsQ8BJPx21NDiZloaCoyfetjjPkRP4'
+  //       },
+  //       body: jsonEncode(
+  //         <String, dynamic>{
+  //           'priority': 'high',
+  //           'data': <String, dynamic>{
+  //             'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+  //             'status': 'done',
+  //             'body': 'your appointment has been canceled',
+  //             'title': 'appointment cancelation',
+  //           },
+  //           "notification": <String, dynamic>{
+  //             "title": "appointment cancelation",
+  //             "body": "your appointment has been canceled",
+  //             "android_channel_id": "dbfood",
+  //           },
+  //           "to": token,
+  //         },
+  //       ),
+  //     );
+  //   } catch (e) {
+  //     if (kDebugMode) {
+  //       print("Error push notifcathion");
+  //     }
+  //   }
+  // }
+
+//+++++++++++++++++++++++++++++++++++++++++DEEM+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 }
