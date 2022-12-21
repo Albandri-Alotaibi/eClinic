528e8f2e27aa7c96e96dbb8940616c17aa955d79
DeemAt10 <d-10-16@hotmail.com>
2022-12-21 15:57:36 +0300

    add viewbooked student

---
+++ eclinic_application/lib/StudentViewBookedAppointment.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StudentViewBookedAppointment extends StatefulWidget {
+  const StudentViewBookedAppointment({super.key});
+
+  @override
+  State<StudentViewBookedAppointment> createState() =>
+      _StudentViewBookedAppointmentState();
+}
+
+class _StudentViewBookedAppointmentState
+    extends State<StudentViewBookedAppointment> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
