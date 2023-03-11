0dc3aef4a812574cf71878ed993c49c26c4eb874
DeemAt10 <d-10-16@hotmail.com>
2023-03-11 21:43:49 +0300

    Update snackbar.dart

--- eclinic_application/lib/screeens/resources/snackbar.dart
+++ eclinic_application/lib/screeens/resources/snackbar.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
-
-void showInSnackBar(context , String value , {color = Colors.green , bool onError = false  }) {
+void showInSnackBar(context, String value,
+    {color = Colors.green, bool onError = false}) {
   try {
-    ScaffoldMessenger.of(context).showSnackBar(
-
-        SnackBar(
-            backgroundColor: onError ? Colors.red :  color,
-            content: Text( (onError ? 'Error : ' : '') + value, style: Theme
-                .of(context)
-                .textTheme
-                .button
-                ?.copyWith(fontSize: 15, color: Colors.white ), textAlign: TextAlign.center,)));
-  }catch(e){
-    debugPrint('error from snack bar ') ;
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        backgroundColor: onError ? Colors.red : color,
+        content: Row(
+          children: [
+            Icon(
+              color: Colors.white,
+              onError
+                  ? Icons.report_problem_outlined
+                  : Icons.check_circle_outline_rounded,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Text(
+              (onError ? 'Error : ' : '') + value,
+              style: Theme.of(context)
+                  .textTheme
+                  .button
+                  ?.copyWith(fontSize: 15, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        )));
+  } catch (e) {
+    debugPrint('error from snack bar ');
   }
 }
