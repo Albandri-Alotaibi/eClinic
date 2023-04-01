e3ecbad1a5647d711bebf020b53d72ae5fc91151
Alhanouf12 <93281161+Alhanouf12@users.noreply.github.com>
2023-04-01 03:58:48 +0300

    Update facultysignup.dart

--- eclinic_application/lib/facultysignup.dart
+++ eclinic_application/lib/facultysignup.dart
@@ -555,11 +555,11 @@ class _facultysignupState extends State<facultysignup> {
                                 if (value!.isEmpty ||
                                     _emailController.text == "") {
                                   return 'Please enter your email ';
-                                  // } else {
-                                  //   if (!(ksuEmailRegEx
-                                  //       .hasMatch(_emailController.text))) {
-                                  //     return 'Please write email format correctly, example@ksu.edu.sa ';
-                                  //   }
+                                } else {
+                                  if (!(ksuEmailRegEx
+                                      .hasMatch(_emailController.text))) {
+                                    return 'Please write email format correctly, example@ksu.edu.sa ';
+                                  }
                                 }
                               },
                             ),
