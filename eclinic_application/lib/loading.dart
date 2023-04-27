453bbf604a4198a051930b260d632bf37f6c572a
DeemAt10 <d-10-16@hotmail.com>
2023-04-27 21:21:08 +0300

    Update loading.dart

--- eclinic_application/lib/loading.dart
+++ eclinic_application/lib/loading.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/UploadGPG.dart';
 import 'package:myapp/graduatehome.dart';
@@ -11,6 +13,7 @@ class Loading extends StatefulWidget {
 }
 
 class _Loading extends State<Loading> {
+  bool contain = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,19 +23,64 @@ class _Loading extends State<Loading> {
                 color: Mycolors.mainShadedColorBlue)));
   }
 
+  String? userid = '';
   @override
   void initState() {
     super.initState;
-    cry();
+    repet();
+  }
+
+  bool found = false;
+  repet() async {
+    while (!found) {
+      await Future.delayed(Duration(seconds: 1));
+      cry();
+    }
   }
 
   cry() async {
-    await Future.delayed(Duration(seconds: 1));
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => graduatehome(),
-      ),
-    );
+    //await Future.delayed(Duration(seconds: 2));
+    print("in cry");
+    final FirebaseAuth auth = await FirebaseAuth.instance;
+    final User? user = await auth.currentUser;
+    userid = user!.uid;
+    final snap = await FirebaseFirestore.instance
+        .collection("graduate")
+        .doc(userid)
+        .get();
+    var group = await snap['group'];
+    final DocumentSnapshot groupRef = await group.get();
+    // var GPname = groupRef['projectname'];
+    // var groupid = groupRef.id;
+    print("befor snap3");
+    final snap3 = await FirebaseFirestore.instance
+        .collection("GPlibrary")
+        .where("group", isEqualTo: group)
+        .get()
+        .then((QuerySnapshot snapshot) {
+      print("befor forEach");
+      snapshot.docs.forEach((DocumentSnapshot doc) async {
+        print("befor snapK");
+        final snapK = doc.data() as Map<String, dynamic>;
+        print("befor containsKey");
+        if (snapK.containsKey("FileUrl")) {
+          found = true;
+          // if (snap["FileUrl"] != null)
+
+          print("جا الزفت");
+        }
+      });
+    });
+
+    if (found == true) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => graduatehome(),
+        ),
+      );
+    }
   }
+
+
 }
